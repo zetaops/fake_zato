f1d277c58f80a352b3715c145ce55a4030a4ab6a
Ali Riza Keles <ali.r.keles@gmail.com>
2016-01-26 16:00:28 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='Fake Zato',
+    version='0.1.0',
+    description='Fake Zato',
+    author='Zetaops',
+    author_email='aliriza@zetaops.io',
+    url='https://github.com/zetaops/fake_zato',
+    packages=find_packages(),
+)
+
